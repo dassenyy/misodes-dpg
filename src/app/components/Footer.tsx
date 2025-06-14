4e7b1842286464c5ffb32951d936fed138fdecda
Dassen <65651014+dassenyy@users.noreply.github.com>
2025-06-14 18:04:46 +0200

    Updated Footer content and style

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,13 +12,19 @@ export function Footer({ donate }: Props) {
 		<p>
 			<span>{locale('developed_by')} <a href="https://github.com/misode" target="_blank" rel="noreferrer">Misode</a></span>
 		</p>
+		<p>
+			<span>{locale('footer.fork_by')} <a href="https://github.com/dassenyy" target="_blank" rel="noreferrer">dassenyy</a></span>
+		</p>
 		{donate !== false && <p class="donate">
 			{Octicon.heart}
-			<a href="https://ko-fi.com/misode" target="_blank" rel="noreferrer">{locale('donate')}</a>
+			<span>{locale('footer.donate_misode')} <a href="https://ko-fi.com/misode" target="_blank" rel="noreferrer">{locale('donate')}</a></span>
 		</p>}
 		<p>
 			{Octicon.mark_github}
 			<span>{locale('source_code_on')} <a href={SOURCE_REPO_URL} target="_blank" rel="noreferrer">{locale('github')}</a></span>
+		</p>
+		<p>
+			<a href="/legal-notice/">{locale('footer.legal_notice')}</a>
 		</p>
 	</footer>
 }
